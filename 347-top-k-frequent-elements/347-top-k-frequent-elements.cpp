ec4b5cf5d6dec4370355f8433210dd921469e581
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-05-12 17:05:04 +0530

    Time: 11 ms (97.00%), Space: 15.1 MB (8.25%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>> maxHeap;
-        map<int,int> hash;
-        vector<int> ans;
-        for(int i = 0; i < nums.size(); i++)
-            hash[nums[i]]++;
-        for(auto elem : hash)
-            maxHeap.push({elem.second, elem.first});
-        while(k--){
-            ans.push_back(maxHeap.top().second);
-            maxHeap.pop();
+        vector<int> buckets[nums.size() + 1];
+        unordered_map<int,int> hash;
+        for(int elems : nums){
+            hash[elems]++;
         }
+        for(auto elems : hash){
+            buckets[elems.second].push_back(elems.first);
+        }
+        vector<int> ans;
+        for(int freq = nums.size(); freq > 0 && ans.size() < k; freq--){
+                ans.insert(ans.end(), buckets[freq].begin(), buckets[freq].end());
+        }
+        ans.resize(k);
         return ans;
     }
 };
+
+//using bucket sort
