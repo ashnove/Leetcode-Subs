c1b39ff583997da8bfbdd1db93b8a9a744961900
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-06-12 00:59:37 +0530

    Time: 488 ms (40.00%), Space: 16.8 MB (40.00%) - LeetHub

--- 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
+++ 2301-match-substring-after-replacement/2301-match-substring-after-replacement.cpp
@@ -10,24 +10,17 @@ public:
         }
         int n = s.size();
         int m = sub.size();
-        // cout << n << " " << m << endl;
         bool ans = 0;
-        for(int i = 0; i < n; i++){
-            if(i + m - 1 >= n) break;
+        for(int i = 0; i + m - 1 < n; i++){
             int k = i;
             bool ok = 1;
             for(int j = 0; j < m; j++, k++){
                 if(sub[j] != s[k]){
-                    if(revMap[s[k] - '0'][sub[j] - '0']){
-                        // temp[s[k]] = '#';
-                    }
-                    else {
+                    if(!revMap[s[k] - '0'][sub[j] - '0']){
                         ok = 0;
                     }
                 }
             }
-                // if(ok)
-                // cout << i << " " << ok << endl;
             ans |= ok;
         }
         return ans;
