c16b68cc49444dd905a3b8d0a69ea12d4bc1cc15
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-06-22 23:57:42 +0530

    Time: 22 ms (71.61%), Space: 16.6 MB (80.93%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -5,13 +5,10 @@ public:
         if(nums.size() == 1) return 0;
         for(int i = 0; i < nums.size() - 1; i++){
             maxReach = max(maxReach, i + nums[i]);
-            
             if(i == curEnd){
-                // cout << i << endl;
                 jump++;
                 curEnd = maxReach;
             }
-
         }
         return jump;
     }
