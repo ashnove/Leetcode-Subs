20ee4f2431f51d7a06ce617445a1089bd22019d7
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-06-29 00:17:37 +0530

    Time: 33 ms (6.56%), Space: 12.1 MB (20.33%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    bool isOperation(string token){
-        return (token=="+"||token=="-"||token=="/"||token=="*");
-    }
-    int performOperation(int num1, int num2, string operand){
-        switch(operand[0]){
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num2 - num1;
-            case '*':
-                return num2 * num1;
-            default:
-                return num2 / num1;
-        }
-    }
+    
     int evalRPN(vector<string>& tokens) {
+        unordered_map<string, function<int (int, int)>> operation = {
+            { "+", [](int a, int b) { return a + b; } },
+            { "-", [](int a, int b) { return a - b; } },
+            { "/", [](int a, int b) { return a / b; } },
+            { "*", [](int a, int b) { return a * b; } },
+        };
         stack<int> s;
         for(string token : tokens){
-            if(isOperation(token)){
+            if(operation.count(token)){
                 int num1 = s.top(); s.pop();
                 int num2 = s.top(); s.pop();
-                s.push(performOperation(num1, num2, token));
+                s.push(operation[token](num2, num1));
             }
             else {
                 s.push(stoi(token));
