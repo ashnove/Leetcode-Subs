5bb5c878475fe9327012054239e000962a4b690f
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-06-29 20:12:04 +0530

    Time: 6 ms (26.91%), Space: 6 MB (51.30%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t rev = 0;
-        int idx = 31;
         for(int i = 0; i < 32; i++){
-            rev += (1 << idx) * ((1 << i) & n ? 1 : 0);
-            idx--;
+            rev <<= 1;
+            if(n & 1) rev|=1;
+            n >>= 1;
         }
         return rev;
     }
