1f07d2959d93f8f182a8a153601f3c1df96ee642
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-06-29 19:35:19 +0530

    Time: 14 ms (18.19%), Space: 7.9 MB (72.89%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
         vector<int> dp(n + 1);
         for(int i = 0; i <= n; i++){
             dp[i] = dp[i / 2] + i % 2;
-            res.push_back(dp[i]);
         }
-        return res;
+        return dp;
     }
 };
