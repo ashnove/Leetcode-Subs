e011c8d1b94d6dfb6a2d71de92b9ea098422e08b
Ashutosh Kumar Shaw <55032392+ashnove@users.noreply.github.com>
2022-07-15 09:50:18 +0530

    Time: 31 ms (51.00%), Space: 23.3 MB (48.63%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -4,14 +4,15 @@ public:
     bool isInRange(int i, int j, int row, int col){
         return (i >= 0 && i < row && j >= 0 && j < col);        
     }
-    int getIslandSize(int i, int j, int row, int col, vector<vector<int>>& grid){
+    int getIslandSize(int i, int j, vector<vector<int>>& grid){
         grid[i][j] = 0;
         int result = 0;
+        int row=grid.size(), col=grid[0].size(); 
         for(int d = 0; d < 4; d++){
             int next_i = i + dir[d];
             int next_j = j + dir[d + 1];
             if(isInRange(next_i, next_j, row, col) && grid[next_i][next_j]){
-                result += 1 + getIslandSize(next_i, next_j, row, col, grid);
+                result += 1 + getIslandSize(next_i, next_j, grid);
             }
         }
         return result;
@@ -23,7 +24,7 @@ public:
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(grid[i][j]){
-                    maxarea = max(maxarea, 1 + getIslandSize(i, j, row, col, grid));
+                    maxarea = max(maxarea, 1 + getIslandSize(i, j, grid));
                 }
             }
         }
